ff8807901a6d29e4817ed50fc081bb10c7304a0b
YunhoLee <yl4643@nyu.edu>
2025-06-11 16:44:45 +0900

    250611_Renpy

---
+++ My project/Assets/Data/Dialogue1DB.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class Dialogue1DB : ScriptableObject
+{
+	public List<Dialogue1Entity> Dialogue_Tutorial; // Replace 'EntityType' to an actual type that is serializable.
+}
---
+++ My project/Assets/Data/Entity/ImageEntity.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class CharImageEntity
+{
+    public int CharID;
+    public int ImgID;
+    public string Name;
+}
